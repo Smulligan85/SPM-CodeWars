5669f2dd23302102033f5f8207f15962cd3473a7
Sean Mulligan <sean.mulligan85@gmail.com>
2015-12-11 07:52:56 -0800

    counting-in-the-amazon

---
+++ counting_in_the_amazon.rb
@@ -0,0 +1,13 @@
+# Counting in the Amazon
+
+def count_arara(n)
+    num = n / 2
+    if n < 2
+      "anane"
+      elsif n % 2 === 0
+        ["adak"] * num * " "
+      else
+         "adak " * num + "anane"
+    end
+end
+
